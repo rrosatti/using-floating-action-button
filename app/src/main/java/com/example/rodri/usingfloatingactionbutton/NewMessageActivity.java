8d74285b39e6d542b259ecbceb616288e57734e5
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-07-02 08:38:50 -0300

    create NewMessageActivity.java and activity_new_message.xml

---
+++ app/src/main/java/com/example/rodri/usingfloatingactionbutton/NewMessageActivity.java
@@ -0,0 +1,16 @@
+package com.example.rodri.usingfloatingactionbutton;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rodri on 7/2/2016.
+ */
+public class NewMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_message);
+    }
+}
