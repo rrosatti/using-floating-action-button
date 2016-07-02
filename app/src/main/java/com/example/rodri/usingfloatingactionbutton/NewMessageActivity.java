2a683eda887669aad787b2b2dd789d9561558a42
Rodrigo <rodrigo_rosatti@hotmail.com>
2016-07-02 13:42:23 -0300

    create circle_2.xml. Make custom Floating Action Button works.

--- app/src/main/java/com/example/rodri/usingfloatingactionbutton/NewMessageActivity.java
+++ app/src/main/java/com/example/rodri/usingfloatingactionbutton/NewMessageActivity.java
@@ -1,16 +1,43 @@
 package com.example.rodri.usingfloatingactionbutton;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 /**
  * Created by rodri on 7/2/2016.
  */
 public class NewMessageActivity extends AppCompatActivity {
 
+    private ImageButton imgCustomFAB;
+    private View.OnClickListener onClickListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_message);
+
+        imgCustomFAB = (ImageButton) findViewById(R.id.imgCustomFAB);
+        imgCustomFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(findViewById(android.R.id.content), "Using Floating Action Button!", Snackbar.LENGTH_LONG)
+                        .setAction("Got it!", onClickListener)
+                        .setActionTextColor(Color.RED)
+                        .show();
+            }
+        });
+
+        onClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "It worked!", Toast.LENGTH_SHORT).show();
+            }
+        };
+
     }
 }
